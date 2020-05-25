8335d94548882e0633b55906cc4fdfff12899231
kevin <kevin345618@outlook.com>
2020-05-25 15:33:50 +0800

    test pokemon build 🤔

---
+++ views/pokemon/precache-manifest.94d0dd37b1a9f17a5005e65ecff70894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15190ef78d95b4d8c56326b6a8e7757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "685aeb32bfa614bd213c",
+    "url": "/static/css/main.49dea0c0.chunk.css"
+  },
+  {
+    "revision": "220541ac3b8bc840acae",
+    "url": "/static/js/2.59091356.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.59091356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685aeb32bfa614bd213c",
+    "url": "/static/js/main.a4a7f6d8.chunk.js"
+  },
+  {
+    "revision": "9a88df53cf648a5f66fd",
+    "url": "/static/js/runtime-main.2d2c88fa.js"
+  },
+  {
+    "revision": "f3a75f76bb8ef7e948b93df37f406fd9",
+    "url": "/static/media/ball-icon.f3a75f76.png"
+  }
+]);
